a5736caa1e30e507c6e8eb8e3473b6268b5bc014
王璐 <568818216@qq.com>
2021-09-12 14:46:57 +0800

    cookie传值

--- 花店网站/js/cookie_utils.js
+++ 花店网站/js/cookie_utils.js
@@ -1,24 +1,24 @@
-	function setCookieValue(name, value) {
- var Days = 1;
-    var exp = new Date();
-    exp.setTime(exp.getTime() + Days*24*60*60*1000);
-    document.cookie = name + "="+value + ";path=/";
-	}
- 
-	
- 
- 
-	//获取 cookie 值的函数,创建一个函数用户返回指定 cookie 的值
-	//cname:名称，你之前设置的名称
-	function getCookieValue(name){//获取指定名称的cookie的值 
-	var cookies = document.cookie;
-	 var list = cookies.split(";");     // 解析出名/值对列表
-	      
-	 for(var i = 0; i < list.length; i++) {
-	 var arr = list[i].split("=");   // 解析出名和值
-	 if(arr[0] == name)
-	 return decodeURIComponent(arr[1]);   // 对cookie值解码
-	 } 
-	 return "";
+var operator = "=";
 
+function getCookieValue(keyStr){
+	var value = null;
+	var s = window.document.cookie;
+	var arr = s.split("; ");
+	for(var i=0; i<arr.length; i++){
+		var str = arr[i];
+		var k = str.split(operator)[0];
+		var v = str.split(operator)[1];
+		if(k == keyStr){
+			value = v;
+			break;
+		}
 	}
+	return value;
+}
+
+function setCookieValue(key,value){
+	document.cookie = key+operator+value;
+}
+function clearCookie(name) {
+    setCookieValue(name, "");
+}
